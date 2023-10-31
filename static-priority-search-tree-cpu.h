d49e380b77173eb9dad8cfa6472d87990028d1f1
Brian <bc2611@nyu.edu>
2023-10-31 19:24:57 -0400

    Skeleton of class

--- static-priority-search-tree-cpu.h
+++ static-priority-search-tree-cpu.h
@@ -4,11 +4,25 @@
 class StaticPrioritySearchTreeCPU
 {
 	public:
+		StaticPrioritySearchTreeCPU(int *search_keys, int num_search_keys, int *priorities, int num_priorities);
+		~StaticPrioritySearchTreeCPU();
+		// Returns a list of satisfying nodes; maybe the array index of each satisfying node?
+		ThreeSidedSearch();
+		TwoSidedLeftSearch();
+		TwoSidedRightSearch();
 
 	private:
 		// Want unique copies of each tree, so no assignment or copying allowed
-		StaticPrioritySearchTreeCPU& operator=(StaticPrioritySearchTreeCPU &tree)	// assignment operator
-		StaticPrioritySearchTreeCPU(PrioritySearchTreeCPU &tree)	// copy constructor
+		StaticPrioritySearchTreeCPU& operator=(StaticPrioritySearchTreeCPU &tree);	// assignment operator
+		StaticPrioritySearchTreeCPU(PrioritySearchTreeCPU &tree);	// copy constructor
+
+		class Node
+		{
+			int search_key;
+			int priority;
+			int median_search_key;
+			bool is_leaf;
+		};
 }
 
 #endif
