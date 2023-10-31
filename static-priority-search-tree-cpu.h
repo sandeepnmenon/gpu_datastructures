ab2cf9f5e79d9102c928cbcd3a272a1e1d3168e4
Brian <bc2611@nyu.edu>
2023-10-31 19:15:38 -0400

    Updated filename

---
+++ static-priority-search-tree-cpu.h
@@ -0,0 +1,14 @@
+#ifndef PRIORITY_SEARCH_TREE_CPU_H
+#define PRIORITY_SEARCH_TREE_CPU_H
+
+class StaticPrioritySearchTreeCPU
+{
+	public:
+
+	private:
+		// Want unique copies of identical trees, so no assignment or copying allowed
+		StaticPrioritySearchTreeCPU& operator=(StaticPrioritySearchTreeCPU &tree)	// assignment operator
+		StaticPrioritySearchTreeCPU(PrioritySearchTreeCPU &tree)	// copy constructor
+}
+
+#endif
