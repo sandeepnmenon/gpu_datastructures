da8eb01787d2dc44c682380d423fc44c904f3487
mrossigrad <95657477+mrossigrad@users.noreply.github.com>
2023-12-12 14:29:53 -0500

    cg insert fix (#7)
    * cg insert fix
    
    ---------
    
    Co-authored-by: mrossigrad <mrossigrad@gmail.com>
    Co-authored-by: Sandeep Menon <menonsandu@gmail.com>

--- basics/basic_hashmap.cu
+++ basics/basic_hashmap.cu
@@ -143,7 +143,7 @@ __device__ bool Hashmap<Key, Value>::insert(cg::thread_block_tile<4> group, Key 
     // get initial probing position from the hash value of the key
     auto i = (hash_custom(k) + group.thread_rank()) % capacity;
     auto state = probing_state::CONTINUE;
-    printf("inserting key:%d value:%d i: %d\n", k, v, i);
+    //printf("inserting key:%d value:%d i: %d\n", k, v, i);
 
     while (true)
     {
@@ -159,7 +159,7 @@ __device__ bool Hashmap<Key, Value>::insert(cg::thread_block_tile<4> group, Key 
         {
             // elect a candidate rank (here: thread with lowest rank in mask)
             auto const candidate = __ffs(empty_mask) - 1;
-            printf("candidate: %d, rank: %d key: %d value: %d i: %d\n", candidate, group.thread_rank(), k, v, i);
+            //printf("candidate: %d, rank: %d key: %d value: %d i: %d\n", candidate, group.thread_rank(), k, v, i);
             if (group.thread_rank() == candidate)
             {
                 // attempt atomically swapping the input Pair into the bucket
@@ -168,20 +168,20 @@ __device__ bool Hashmap<Key, Value>::insert(cg::thread_block_tile<4> group, Key 
                     old_kv, desired, std::memory_order_relaxed);
                 if (success)
                 {
-                    printf("inserted key:%d value:%d i: %d\n", k, v, i);
+                    //printf("inserted key:%d value:%d i: %d\n", k, v, i);
                     // insertion went successful
                     state = probing_state::SUCCESS;
                 }
                 else if (old_kv.first == k)
                 {
-                    printf("duplicate key:%d value:%d i: %d\n", k, v, i);
+                    //printf("duplicate key:%d value:%d i: %d\n", k, v, i);
                     // else, re-check if a duplicate key has been inserted at the current probing position
                     state = probing_state::DUPLICATE;
                 }
             }
             // broadcast the insertion result from the candidate rank to all other ranks
             auto const candidate_state = group.shfl(state, candidate);
-            printf("candidate_state: %d key: %d value: %d i: %d\n", candidate_state, k, v, i);
+            //printf("candidate_state: %d key: %d value: %d i: %d\n", candidate_state, k, v, i);
             if (candidate_state == probing_state::SUCCESS)
                 return true;
             if (candidate_state == probing_state::DUPLICATE)
@@ -189,7 +189,7 @@ __device__ bool Hashmap<Key, Value>::insert(cg::thread_block_tile<4> group, Key 
         }
         else
         {
-            printf("continuing key:%d value:%d i: %d\n", k, v, i);
+            //printf("continuing key:%d value:%d i: %d\n", k, v, i);
             // else, move to the next (linear) probing window
             i = (i + group.size()) % capacity;
         }
--- basics/driver.cu
+++ basics/driver.cu
@@ -1,23 +1,56 @@
 #include <iostream>
+#include <iomanip>
 #include <map>
 #include <unistd.h>
 #include <functional>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <cooperative_groups.h>
+#include <thrust/sequence.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
-#include <thrust/sequence.h>
+#include <cassert>
 
 #include "basic_hashmap.cu"
 #include "utils.cuh"
 
 namespace cg = cooperative_groups;
 
-__global__ void testIntInsertCG(const int *keys, const int *values, const size_t numElements, Hashmap<int, int> *hashmap)
+struct Config {
+
+    bool defaultInsert = false;
+    bool cooperativeGroupsInsert = false;
+    bool defaultSearch = false;
+    size_t device = 2;
+    size_t threads = 1;
+    size_t blocks = 1;
+    size_t numElements = 1;
+    static constexpr size_t cg_size = 4;
+    float load = 1.0f;
+
+} config;
+
+std::map<char, std::function<void(const char*)>> actions;
+
+__global__ void testIntInsertCG(const int *keys, const int *values, const size_t numElements, Hashmap<int, int> *hashmap, size_t cg_size)
 {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    int idx = (threadIdx.x + blockIdx.x * blockDim.x) / cg_size;
     if (idx < numElements)
+    {
+        auto group = cg::tiled_partition<4>(cg::this_thread_block());
+        if (!hashmap->insert(group, keys[idx], values[idx]))
+        {
+            printf("Insertion failed for key[%d] %d\n", idx, keys[idx]);
+        }
+    }
+}
+
+__global__ void testIntInsertCG_2(const int *keys, const int *values, const size_t numElements, Hashmap<int, int> *hashmap, size_t cg_size)
+{
+    int threadId = (threadIdx.x + blockIdx.x * blockDim.x) / cg_size;
+    int totalThreads = (gridDim.x * blockDim.x) / cg_size; // Total number of active threads
+
+    for (int idx = threadId; idx < numElements; idx += totalThreads)
     {
         auto group = cg::tiled_partition<4>(cg::this_thread_block());
         if (!hashmap->insert(group, keys[idx], values[idx]))
@@ -36,25 +69,79 @@ __global__ void testIntInsert(const int *keys, const int *values, const size_t n
     }
 }
 
+__global__ void testIntInsert_2(const int *keys, const int *values, const size_t numElements, Hashmap<int, int> *hashmap)
+{
+    int threadId = threadIdx.x + blockIdx.x * blockDim.x; // unique thread id
+    int totalThreads = gridDim.x * blockDim.x; // Total number of active threads
+
+    // Loop over elements. Each thread handles multiple insertions.
+    for (int idx = threadId; idx < numElements; idx += totalThreads)
+    {
+        hashmap->insert(keys[idx], values[idx]);
+    }
+}
+
 void insertionBenchmarkFunc(Hashmap<int, int> *hashmap, const thrust::device_vector<int> &d_keys, const thrust::device_vector<int> &d_values)
 {
     // Define grid and block sizes
-    int numElements = d_keys.size();
-    int blockSize = 256;
+    int numElements = config.numElements;
+    int blockSize = config.threads;
     int gridSize = (numElements + blockSize - 1) / blockSize;
+
+    std::cout << std::setw(25) << "Threads per block:" << blockSize << "\n";
+    std::cout << std::setw(25) << "Number of blocks:" << gridSize << "\n";
+    std::cout << std::setw(25) << "Total number of threads:" <<  blockSize * gridSize << "\n";
 
     testIntInsert<<<gridSize, blockSize>>>(thrust::raw_pointer_cast(d_keys.data()), thrust::raw_pointer_cast(d_values.data()), numElements, hashmap);
     cudaDeviceSynchronize();
 }
 
-void insertionBenchmarkCGFunc(Hashmap<int, int> *hashmap, const thrust::device_vector<int> &d_keys, const thrust::device_vector<int> &d_values)
+void insertionBenchmarkFunc_2(Hashmap<int, int> *hashmap, const thrust::device_vector<int> &d_keys, const thrust::device_vector<int> &d_values)
 {
     // Define grid and block sizes
-    int numElements = d_keys.size();
-    int blockSize = 256;
-    int gridSize = (numElements + blockSize - 1) / blockSize;
+    int numElements = config.numElements;
+    int blockSize = config.threads;
+    int gridSize = config.blocks;
 
-    testIntInsertCG<<<gridSize, blockSize>>>(thrust::raw_pointer_cast(d_keys.data()), thrust::raw_pointer_cast(d_values.data()), numElements, hashmap);
+    std::cout << std::setw(25) << "Threads per block:" << blockSize << "\n";
+    std::cout << std::setw(25) << "Number of blocks:" << gridSize << "\n";
+    std::cout << std::setw(25) << "Total number of threads:" <<  blockSize * gridSize << "\n";
+
+    testIntInsert_2<<<gridSize, blockSize>>>(thrust::raw_pointer_cast(d_keys.data()), thrust::raw_pointer_cast(d_values.data()), numElements, hashmap);
+    cudaDeviceSynchronize();
+}
+
+void insertionBenchmarkCGFunc(Hashmap<int, int> *hashmap, const thrust::device_vector<int> &d_keys, const thrust::device_vector<int> &d_values)
+{
+    // Define default grid and block sizes
+    int numElements = config.numElements;
+    int blockSize = config.threads;
+    int gridSize = (numElements * config.cg_size + blockSize - 1) / blockSize;
+
+    assert(blockSize >= 4); // make sure there are at least 4 threads for the cooperative insert
+
+    std::cout << std::setw(25) << "Threads per block:" << blockSize << "\n";
+    std::cout << std::setw(25) << "Number of blocks:" << gridSize << "\n";
+    std::cout << std::setw(25) << "Total number of threads:" <<  blockSize * gridSize << "\n";
+
+    testIntInsertCG<<<gridSize, blockSize>>>(thrust::raw_pointer_cast(d_keys.data()), thrust::raw_pointer_cast(d_values.data()), numElements, hashmap, config.cg_size);
+    cudaDeviceSynchronize();
+}
+
+void insertionBenchmarkCGFunc_2(Hashmap<int, int> *hashmap, const thrust::device_vector<int> &d_keys, const thrust::device_vector<int> &d_values)
+{
+    // Define default grid and block sizes
+    int numElements = config.numElements;
+    int blockSize = config.threads;
+    int gridSize = config.blocks;
+
+    assert(blockSize >= 4); // make sure there are at least 4 threads for the cooperative insert
+
+    std::cout << std::setw(25) << "Threads per block:" << blockSize << "\n";
+    std::cout << std::setw(25) << "Number of blocks:" << gridSize << "\n";
+    std::cout << std::setw(25) << "Total number of threads:" <<  blockSize * gridSize << "\n";
+
+    testIntInsertCG_2<<<gridSize, blockSize>>>(thrust::raw_pointer_cast(d_keys.data()), thrust::raw_pointer_cast(d_values.data()), numElements, hashmap, config.cg_size);
     cudaDeviceSynchronize();
 }
 
@@ -64,48 +151,58 @@ void searchBenchMarkFunc(Hashmap<int, int> *hashmap, const thrust::device_vector
     cudaDeviceSynchronize();
 }
 
-bool defaultInsert = false;
-bool cooperativeGroupsInsert = false;
-bool defaultSearch = false;
-std::map<char, std::function<void()>> actions;
-
-void setupActions()
-{
-    actions['d'] = [&]()
-    { defaultInsert = true; std::cout << "Default insert\n"; };
-    actions['c'] = [&]()
-    { cooperativeGroupsInsert = true; std::cout << "Cooperative groups insert\n"; };
-    actions['s'] = [&]()
-    { defaultSearch = true; std::cout << "Default search\n"; };
+void setupActions() {
+    actions['d'] = [](const char*) { config.defaultInsert = true; std::cout << "Default insert\n"; };
+    actions['c'] = [](const char*) { config.cooperativeGroupsInsert = true; std::cout << "Cooperative groups insert\n"; };
+    actions['s'] = [](const char*) { config.defaultSearch = true; std::cout << "Default search\n"; };
+    actions['n'] = [](const char* arg) { config.numElements = std::stoul(arg); };
+    actions['l'] = [](const char* arg) { config.load = std::stof(arg); };
+    actions['t'] = [](const char* arg) { config.threads = std::stoul(arg); };
+    actions['b'] = [](const char* arg) { config.blocks = std::stoul(arg); };
+    actions['g'] = [](const char* arg) { config.device = std::stoul(arg); };
 }
 
 int main(int argc, char **argv)
-{
+{   
+    // Set CUDA Device - Ensure this is valid for your system
+    cudaError_t cudaStatus = cudaSetDevice(config.device);
+    if (cudaStatus != cudaSuccess) {
+        std::cerr << "cudaSetDevice failed!" << std::endl;
+        return 1;
+    }
+
     setupActions();
 
     int opt;
-    while ((opt = getopt(argc, argv, "dcs")) != -1)
-        if (actions.find(opt) != actions.end())
-            actions[opt]();
+    while ((opt = getopt(argc, argv, "dcsn:l:t:b:g:")) != -1) {
+        auto action = actions.find(opt);
+        if (action != actions.end())
+            action->second(optarg);
         else
-            std::cerr << "Unknown option: " << char(opt) << std::endl;
+            std::cerr << "Unknown option: " << static_cast<char>(opt) << '\n';
+    }
+
+    // Define hashmap capacity
+    std::size_t const capacity = std::ceil(config.numElements / config.load);
+
+    std::cout << std::left; // Align text to the left
+    std::cout << std::setw(25) << "Using device:" << config.device << "\n";
+    std::cout << std::setw(25) << "Elements to insert:" << config.numElements << "\n";
+    std::cout << std::setw(25) << "Load factor:" << config.load << "\n";
+    std::cout << std::setw(25) << "Capacity:" << capacity << "\n";
 
     // Initialize data
-    const size_t numElements = 10; // Adjust as needed
-    thrust::host_vector<int> h_keys(numElements), h_values(numElements);
+    thrust::host_vector<int> h_keys(config.numElements), h_values(config.numElements);
 
     // Fill keys and values with test data
-    initializeData(h_keys, numElements);
-    initializeData(h_values, numElements);
+    initializeData(h_keys, config.numElements);
+    initializeData(h_values, config.numElements);
 
     // Copy data from host to device
     thrust::device_vector<int> d_keys = h_keys;
     thrust::device_vector<int> d_values = h_values;
 
     // Create and initialize hashmap
-    auto constexpr load_factor = 0.5;
-    std::size_t const capacity = std::ceil(numElements / load_factor);
-
     Hashmap<int, int> *hashmap; // Assuming constructor initializes the GPU memory
     cudaMallocManaged(&hashmap, sizeof(Hashmap<int, int>));
     new (hashmap) Hashmap<int, int>(capacity);
@@ -116,17 +213,19 @@ int main(int argc, char **argv)
         // handle error
     }
 
-    if (defaultInsert)
+    if (config.defaultInsert)
         benchmarkKernel([&]()
-                        { insertionBenchmarkFunc(hashmap, d_keys, d_values); },
-                        "non-CG Insertion");
+                        { insertionBenchmarkFunc_2(hashmap, d_keys, d_values); },
+                         "non-CG Insertion");
+        
 
-    if (cooperativeGroupsInsert)
+    if (config.cooperativeGroupsInsert)
         benchmarkKernel([&]()
-                        { insertionBenchmarkCGFunc(hashmap, d_keys, d_values); },
+                        { std::cout << std::setw(25) << "Cooperative group size:" << config.cg_size << "\n";
+                          insertionBenchmarkCGFunc_2(hashmap, d_keys, d_values); },
                         "Insertion CG");
 
-    if (defaultSearch)
+    if (config.defaultSearch)
     {
         thrust::device_vector<int> d_results(d_keys.size());
         benchmarkKernel([&]()
